7f2f314a50cc390554749153ad3258a6ebbef101
Ambitious905837 <tj905837@outlook.com>
2024-04-18 01:31:08 +0800

    index.js updated

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 require('dotenv').config();
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
-
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -13,6 +7,12 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
 const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://cloud:0YsOYJne8WL2aTBJ@serverlessinstance0.cys2vnq.mongodb.net/aistudio?retryWrites=true&w=majority&appName=ServerlessInstance0"
 
