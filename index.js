d20451f236e259322c1307ad6e04cd1198b6682b
Ambitious905837 <tj905837@outlook.com>
2024-04-18 02:14:09 +0800

    index.js updated

--- index.js
+++ index.js
@@ -7,14 +7,9 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }
 const PORT = process.env.PORT || 8080;
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://cloud:0YsOYJne8WL2aTBJ@serverlessinstance0.cys2vnq.mongodb.net/aistudio?retryWrites=true&w=majority&appName=ServerlessInstance0"
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://tj905837:hXH68brXRc0QoYyi@aistudio.pkmenaj.mongodb.net/"
 
 // Middlewares
 app.use(cors());
